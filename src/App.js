fffb689e17bbfdef4730f3a0c59d1b2b3b3331b0
itbeard <iamitbeard@gmail.com>
2024-08-05 14:31:15 +0200

    update

--- src/App.js
+++ src/App.js
@@ -19,7 +19,7 @@ function Home() {
   };
 
   const extractVideoId = (url) => {
-    const regExp = /^.*(youtu.be\/|v\/|u\/\w\/|embed\/|watch\?v=|\&v=)([^#\&\?]*).*/;
+    const regExp = /^.*(youtu.be\/|v\/|u\/\w\/|embed\/|watch\?v=|&v=)([^#&?]*).*/;
     const match = url.match(regExp);
     return (match && match[2].length === 11) ? match[2] : null;
   };
