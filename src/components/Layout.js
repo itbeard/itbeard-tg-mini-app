0f18c4db00ab3895c3a9acde580a36d808a470cd
itbeard <iamitbeard@gmail.com>
2024-08-05 23:42:34 +0200

    update home link

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,12 +1,19 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import WebApp from '@twa-dev/sdk';
 
 function Layout({ children }) {
+  const homeLink = WebApp.initDataUnsafe?.start_param ? (
+    <a href="https://t.me/yt_opener_bot/open" target="_blank" rel="noopener noreferrer">Главная</a>
+  ) : (
+    <Link to="/">Главная</Link>
+  );
+
   return (
     <div className="app-container">
       <header>
         <nav>
-          <Link to="/">Главная</Link>
+          {homeLink}
         </nav>
       </header>
       <main>{children}</main>
